b2ab1c56d6e0a78cf33bf5616464e6853de03e51
Harshit Mawri <98049556+harshit404540@users.noreply.github.com>
2024-11-03 22:34:35 +0530

    Age_Calculator.py

--- Age_Calculator.py
+++ Age_Calculator.py
@@ -1,32 +1,44 @@
 from datetime import date
 
-try:
-    today = date.today()
-    resolved_date = str(today).split("-")
+def age():
+    try:
+        today = date.today()
+        resolved_date = str(today).split("-")
+    
+        user_year = int(input("Enter Year: "))
+        user_month = int(input("Enter Month: "))
+        user_date = int(input("Enter Date: "))
+    
+        current_year = resolved_date[0]
+        current_month = resolved_date[1]
+        current_date = resolved_date[2]
+    
+        if  int(user_year) > int(current_year):
+            print("Wrong Year")
+        else:
+            year = int(current_year)-int(user_year)
+        if   int(user_month) > int(current_month):
+            month = int(user_month)-int(current_month)
+        else:
+            month = int(current_month)-int(user_month)
+        if    int(user_date) > int(current_date):
+            day = int(user_date)-int(current_date)
+        else:
+            day = int(current_date)-int(user_date)
+    
+    
+        print(f"Your Age is: {year}yrs {month}months {day}days")
 
-    user_year = int(input("Enter Year: "))
-    user_month = int(input("Enter Month: "))
-    user_date = int(input("Enter Date: "))
+    except Exception as e:
+        print(str(e))
+        
 
-    current_year = resolved_date[0]
-    current_month = resolved_date[1]
-    current_date = resolved_date[2]
-
-    if  int(user_year) > int(current_year):
-        print("Wrong Year")
-    else:
-        year = int(current_year)-int(user_year)
-    if   int(user_month) > int(current_month):
-        month = int(user_month)-int(current_month)
-    else:
-        month = int(current_month)-int(user_month)
-    if    int(user_date) > int(current_date):
-        day = int(user_date)-int(current_date)
-    else:
-        day = int(current_date)-int(user_date)
-
-
-    print(f"Your Age is: {year}yrs {month}months {day}days")
-
-except Exception as e:
-    print(str(e))
+if __name__ == "__main__":
+    while True:
+        print('1. Age Calculator')
+        print('2. Exit')
+        user = int(input("Enter option: "))
+        if user == 1:
+            age()
+        elif user == 2:
+            break
